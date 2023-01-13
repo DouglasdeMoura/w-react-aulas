5062be1cd644d79596fbdaae8069103f28f3d22a
Douglas Moura <douglas.ademoura@gmail.com>
2023-01-13 19:19:12 -0300

    feat: adiciona boilerplate da lista de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
+
+type Tarefa = {
+  id: number
+  texto: string
+  completa: boolean
+}
+
 function App() {
+  const [tarefas, setTarefas] = useState<Tarefa[]>([])
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Tarefas</h1>
+      
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa.id}>{tarefa.texto}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
