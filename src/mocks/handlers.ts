d4ad07f3f68ed30a42b2ef205b0f51994eb70b25
Douglas Moura <douglas.ademoura@gmail.com>
2023-01-13 19:55:56 -0300

    feat: adiciona mock da rota POST /tasks (adicionar tarefa)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,26 +1,33 @@
 import { rest } from "msw";
 
+const tasks = [
+  {
+    id: 1,
+    texto: "Comprar pão",
+    completa: false,
+  },
+  {
+    id: 2,
+    texto: "Pagar a conta de luz",
+    completa: false,
+  },
+  {
+    id: 3,
+    texto: "Pintar o portão",
+    completa: false,
+  },
+];
+
 export const handlers = [
-  rest.get("/tasks", (req, res, ctx) =>
+  rest.get("/tasks", (_req, res, ctx) => res(ctx.status(200), ctx.json(tasks))),
+  rest.post("/tasks", (req, res, ctx) =>
     res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          texto: "Comprar pão",
-          completa: false,
-        },
-        {
-          id: 2,
-          texto: "Pagar a conta de luz",
-          completa: false,
-        },
-        {
-          id: 3,
-          texto: "Pintar o portão",
-          completa: false,
-        },
-      ])
+      ctx.status(201),
+      ctx.json({
+        id: tasks.length + 1,
+        texto: req.params.texto,
+        completa: false,
+      })
     )
   ),
 ];
