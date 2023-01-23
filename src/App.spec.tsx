13ffc257621025de794b0e7a95b31f3ff208540f
Douglas Moura <douglas.ademoura@gmail.com>
2023-01-23 20:05:40 -0300

    wip: corrigir os tipos para o TypeScript verificar o erro na consulta da API durante os testes

---
+++ src/App.spec.tsx
@@ -0,0 +1,39 @@
+import { render, screen, waitFor, waitForElementToBeRemoved } from './utils/test-utils'
+import App from './App'
+
+describe('<App />', () => {
+  it.skip('deve renderizar os componentes com o formulário de adição de tarefas e a lista de tarefas atual', async () => {
+    render(<App />)
+
+    expect(screen.getByRole('heading', { name: 'Tarefas' })).toBeInTheDocument()
+    expect(screen.getByLabelText('Insira a nova tarefa')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Adicionar' })).toBeInTheDocument()
+
+    await waitForElementToBeRemoved(() => screen.getByText('Carregando tarefas'))
+
+
+
+    await waitFor(() => {
+      expect(screen.getByText('Comprar pão')).toBeInTheDocument()
+    })
+    
+  
+    expect(screen.getByText('Pagar a conta de luz')).toBeInTheDocument()
+    expect(screen.getByText('Pintar o portão')).toBeInTheDocument()
+  })
+
+  it.skip('deve adicionar uma nova tarefa', async () => {
+    const { user } = render(<App />)
+
+    const input = screen.getByLabelText('Insira a nova tarefa')
+    const button = screen.getByRole('button', { name: 'Adicionar' })
+
+
+    user.type(input, 'Estudar React')
+    user.click(button)
+
+    await waitFor(() => {
+      expect(screen.getByText('Estudar React')).toBeInTheDocument()
+    })
+  })
+})
