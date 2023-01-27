b22ac33c0650a10debfb8276be85eff61c823c9a
Douglas Moura <douglas.ademoura@gmail.com>
2023-01-27 17:11:33 -0300

    feat: adiciona provider do Mantine nos testes

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -2,14 +2,17 @@
 import userEvent from '@testing-library/user-event'
 import { type RenderOptions, render as rtlRender } from '@testing-library/react'
 import { SWRConfig } from 'swr'
+import { MantineProvider } from '@mantine/core'
 import { api } from './fetcher'
 export * from '@testing-library/react'
 
 export const render = (ui: React.ReactElement, options?: RenderOptions) => {
   const wrapper = ({ children }: { children: React.ReactNode }) => (
-    <SWRConfig value={{ suspense: true, fetcher: api.get }}>
-      {children}
-    </SWRConfig>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <SWRConfig value={{ suspense: true, fetcher: api.get }}>
+        {children}
+      </SWRConfig>
+    </MantineProvider>
   )
 
   return { user: userEvent.setup(), ...rtlRender(ui, { wrapper, ...options }) }
