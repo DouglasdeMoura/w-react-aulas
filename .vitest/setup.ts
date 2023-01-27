553912415ab68f3ba684713b7c873e6c6c583065
Douglas Moura <douglas.ademoura@gmail.com>
2023-01-27 15:59:30 -0300

    feat: adiciona polyfill para o fetch, pois a implementação do Node é diferente da implementação do navegador

--- .vitest/setup.ts
+++ .vitest/setup.ts
@@ -1,12 +1,13 @@
+import { fetch } from 'cross-fetch'
 import * as matchers from 'vitest-dom/matchers'
 import { afterAll, afterEach, beforeAll, expect } from 'vitest'
 
 import { server } from '../src/mocks/server'
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
-
 afterEach(() => server.resetHandlers())
-
 afterAll(() => server.close())
+
+global.fetch = fetch
 
 expect.extend(matchers)
